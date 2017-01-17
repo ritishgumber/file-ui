4dc7c6919b73d1cb14f22132361997b5ccec51bf
Ritish Gumber <cool.ritish@gmail.com>
2017-01-17 23:45:36 +0530

    UI fix

--- dev/js/containers/mainbody.js
+++ dev/js/containers/mainbody.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import {
-    Navbar,
-    NavItem,
-    NavDropdown,
-    MenuItem,
     Nav,
-    Glyphicon
+    Glyphicon,
+    Grid,
+    Row,
+    Col,
+    Modal,
+    Button
 } from 'react-bootstrap';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import DocumentList from '../containers/documentList';
-import {deleteFile, fetchAllFiles, addItem} from '../actions/index';
-
+import {deleteFile, fetchAllFiles, addFile} from '../actions/index';
+import DropZone from './dropzone';
 import {browserHistory, Link} from "react-router";
 
 class MainBody extends Component {
@@ -24,6 +25,33 @@ class MainBody extends Component {
             location: this.props.location.pathname
         };
 
+    }
+    close() {
+        this.setState({showUploadModal: false, showCreateModal: false});
+    }
+    open(type) {
+        this.setState({
+            showUploadModal: (type == 'upload'
+                ? true
+                : false),
+            showCreateModal: (type == 'upload'
+                ? false
+                : true)
+        });
+    }
+    addFolder(e) {
+        const value = (document.getElementById('folderName').value);
+        this.props.addFile({path: this.state.location, file: value, data: 'folder', type: 'folder'});
+
+        this.close();
+    }
+    componentWillUpdate(nextProps, nextState) {
+        if (nextProps.fileAddSuccess)
+            this.props.fetchAllFiles({path: this.state.location});
+        }
+    handleChange(e) {
+        console.log("value:", e.target.value);
+        this.props.fetchAllFiles({path: this.state.location, searchText: e.target.value})
     }
 
     render() {
@@ -40,17 +68,82 @@ class MainBody extends Component {
         let link = "";
         const breadcrumb = a.map((b, i) => {
             link = link + "/" + b;
-            return (
-                <Link key={i} to={link}>
-                    {b}
-                    &gt;
-                </Link>
-            );
-        });
+            if (b != 'home')
+                return (
+                    <Link key={i} to={link}>
+                        {b}
+                        &gt;
+                    </Link>
+                );
+            }
+        );
         return (
-            <div>
-                <div>{breadcrumb}</div>
-                <DocumentList location={location} delete={this.props.routes[1].delete}/>
+            <div class="row">
+                <div className=" col-md-12 ">
+                    <div class="fixed-navbar">
+                        <div class="header-row ">
+                            <a href="#" class="header-elements">
+                                <img src="./assets/upgrade.png" width="15px"/>
+                                &nbsp;Upgrade account&nbsp;&nbsp;
+                            </a>
+                            <a href="#" class="header-elements">
+                                <img src="./assets/notification.png" width="15px"/>
+                            </a>
+                            <a href="#" class="header-elements">
+                                <img src="https://cfl.dropboxstatic.com/static/images/avatar/faceholder-32-vflKWtuU5.png" class="profile-photo" width='24px'/>
+                                &nbsp;My Profile
+                            </a>
+                        </div>
+                        <div >
+                            <span class="inlineLeft">
+                                <h4 class=" inline breadcrumb-row">
+                                    <a href="#">CloudBoost &nbsp;</a>
+                                    {breadcrumb}
+                                </h4>
+                            </span>
+                            <span class="inlineRight">
+                                <img class="inline" onClick={this.open.bind(this, 'upload')} src="./assets/fileadd.png" width="30px"/>
+                                <img class="inline" onClick={this.open.bind(this, 'create')} src="./assets/folderadd.png" width="30px"/>
+                                <img class="inline" src="./assets/delete.png" width="35px"/>
+
+                                <input type="text" class="inline search-bar" onChange={this.handleChange.bind(this)} placeholder="Search"/>
+                            </span>
+                        </div>
+                        <div>
+                            <span>2.pdf
+                            </span>
+                        </div>
+                        <Modal show={this.state.showUploadModal} onHide={this.close.bind(this)}>
+                            <Modal.Header >
+                                <Modal.Title>Upload File</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body >
+                                <DropZone location={this.state.location} close={this.close.bind(this)}/>
+
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={this.close.bind(this)}>Close</Button>
+                            </Modal.Footer>
+                        </Modal>
+                        <Modal show={this.state.showCreateModal} onHide={this.close.bind(this)}>
+                            <Modal.Header >
+                                <Modal.Title>Enter Folder name</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body >
+                                <input className="form-control" id="folderName" placeholder="Enter Folder name"/>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={this.close.bind(this)}>Close</Button>
+                                <Button className="btn-primary" onClick={this.addFolder.bind(this)}>Create</Button>
+
+                            </Modal.Footer>
+                        </Modal>
+
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <DocumentList location={location}/>
+                </div>
             </div>
 
         );
@@ -62,7 +155,8 @@ function mapStateToProps(state) {
 }
 function matchDispatchToProps(dispatch) {
     return bindActionCreators({
-        addItem: addItem
+        addFile: addFile,
+        fetchAllFiles: fetchAllFiles
     }, dispatch);
 }
 export default connect(mapStateToProps, matchDispatchToProps)(MainBody);
