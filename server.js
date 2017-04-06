8e092b5ba3438155749235fe41daf2a9e5ab666c
Shubham Aggarwal <shubhamqweasd@gmail.com>
2017-04-06 16:43:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/app/key.js', function(req, res) {
     content += "ACCOUNTS_URL = null,\n";
     content += "FILE_URL = null,\n";
     content += "LANDING_URL = 'https://www.cloudboost.io';\n";
-    content += "if(window.location.hostname === 'files.cloudboost.io'){\n";
+    content += "if(window.location.hostname.includes('cloudboost.io')){\n";
     content += "USER_SERVICE_URL='https://service.cloudboost.io/';\n";
     content += "SERVER_DOMAIN='cloudboost.io';\n";
     content += "SERVER_URL='https://api.cloudboost.io';\n";
